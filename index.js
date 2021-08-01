e796e7fbe6b4acdc80d73233a11861fba8cc199a
Brandon <88041643+ihcchiistaken@users.noreply.github.com>
2021-08-01 17:27:25 -0400

    Add files via upload
    hehe xd

---
+++ index.js
@@ -0,0 +1,13 @@
+function rickRoll(){
+    for(let i=0; i<5; i++){
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")
+
+    }
+}
+function gotGnomed(){
+    for(let i=0; i<5; i++){
+        window.open("https://www.youtube.com/watch?v=1FFBsX5C61Q&ab_channel=SaucySpaghetti")
+    
+        }
+
+}
