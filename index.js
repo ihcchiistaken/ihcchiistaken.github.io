876edf025f767561482d4dac6313e485c6ee5abe
Brandon Ulloa <brandonulloa@Brandons-Mini.attlocal.net>
2021-08-03 22:01:41 -0400

    added Quack,added franko, boto change color, pictures.

--- index.js
+++ index.js
@@ -11,3 +11,51 @@ function gotGnomed(){
         }
 
 }
+
+function Franko(){
+    let frank = new Audio("/JS/Sounds/oonaniani.mp3");
+    frank.play();    
+
+}
+
+function fontColor(){
+    let colors=["red","blue","green","yellow","purple","orange"]
+    let boto = document.getElementById("name");
+    let rand=randomNumber(colors.length-1)
+    boto.style.color = colors[rand];
+}
+function randomNumber(max){
+    return Math.floor(Math.random()*max)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function duckQuack(){
+    let quack = new Audio("/JS/Sounds/Quack.mp3");
+    quack.play();
+
+    
+}
